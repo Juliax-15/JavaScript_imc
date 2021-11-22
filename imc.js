1cf015fd4cb3319fe69b8c9b9816662ee0f75754
Julia Garcia Mota <79716418+Juliax-15@users.noreply.github.com>
2021-11-22 16:26:56 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,21 @@
+    function calcula_imc(){
+        altura = document.imcform.altura.value
+        peso = document.imcform.peso.value
+        imc = (peso/(altura*altura))
+        
+        if(imc<18.5){
+            alert("Você está abaixo do peso, indíce:"+ imc)
+        }
+        if(imc>=18.5 && imc<=24.99){
+            alert("Você está com o peso ideal, índice:"+ imc)
+        }
+        if(imc>=25 && imc<=29.99){
+            alert("Você está com sobrepeso, índice:"+ imc)
+        }
+        if(imc>=30 && imc<=39.99){
+            alert("Você está com obesidade, índice:"+ imc)
+        }
+        if(imc>=40){
+            alert("Você está com obesidade moórbida, índice:"+ imc)
+        }
+    }
